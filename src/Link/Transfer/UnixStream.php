089f6d73c59af6839608004786b3a4d949284370
Martin Redolatti <martin.redolatti@split.io>
2023-06-09 17:33:06 -0300

    shutdown connectios on destructor invocation

--- src/Link/Transfer/UnixStream.php
+++ src/Link/Transfer/UnixStream.php
@@ -49,4 +49,10 @@ class UnixStream implements RawConnection
         $status = @socket_get_status($this->sock);
         return (!$status['timed_out'] && !$status['eof']);
     }
+
+    public function __destruct()
+    {
+        @socket_close($this->sock);
+    }
+
 }
